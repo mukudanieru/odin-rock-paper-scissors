3ba2e45a93bbcd599179c6f7909581126b3e9e4f
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-09 22:30:40 +0800

    Write the human and computer choice
    - getComputerChoice() - for the random computer choice
    - getHumanChoice() - for the human choice

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+const CHOICES = ["rock", "paper", "scissors"];
+
+function getHumanChoice() {
+    let choice;
+
+    while (true) {
+        const PROMPT_TEXT = `Choose between these:
+        [1] - Rock
+        [2] - Paper
+        [3] - Scissors`;
+    
+        choice = Number(prompt(PROMPT_TEXT));
+        
+        if ((choice - 1) >= (CHOICES.length - CHOICES.length) && (choice - 1) < CHOICES.length) {
+            return CHOICES[choice - 1];
+        }
+
+        alert("Invalid choice: Try again!");
+    }
+}
+
+function getComputerChoice() {
+    const idx = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[idx];
+}
