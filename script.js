2bc6b87d722b4182622b81ab46bf507998853748
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2024-12-10 07:01:13 +0800

    Add the entire game logic

--- script.js
+++ script.js
@@ -3,16 +3,45 @@ const CHOICES = ["rock", "paper", "scissors"];
 let humanScore = 0;
 let computerScore = 0;
 
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
+function playGame() {
+    const ROUNDS = 5;
 
-// playRound(humanSelection, computerSelection);
+    for (let i = 0; i < ROUNDS; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore === computerScore) {
+        console.log("It's a tie!");
+    } else if (humanScore > computerScore) {
+        console.log("You won!");
+    } else {
+        console.log("The computer won!")
+    }
+}
 
 function playRound(humanChoice, computerChoice) {
-    // 
-    // 
-    // 
-    // 
+    alert(`Computer choice: ${computerChoice}`);
+
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! Both chose ${humanChoice}.`);
+        alert(`It's a tie! Both chose ${humanChoice}.`);
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`You won! ${humanChoice} crushes ${computerChoice}.`);
+        alert(`You won! ${humanChoice} crushes ${computerChoice}.`)
+        humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`You won! ${humanChoice} covers ${computerChoice}.`);
+        alert(`You won! ${humanChoice} covers ${computerChoice}.`)
+        humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`You won! ${humanChoice} cut ${computerChoice}.`)
+        alert(`You won! ${humanChoice} cut ${computerChoice}.`);
+        humanScore++;
+    } else {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}.`)
+        alert(`You lost! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
 }
 
 function getHumanChoice() {
